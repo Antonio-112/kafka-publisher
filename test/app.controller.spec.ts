acfc2457ef3693249c4d6e7c22052fe0c155caa2
Antonio Aviles <71448936+Antonio-112@users.noreply.github.com>
2023-04-06 15:44:14 -0400

    Files changed

--- test/app.controller.spec.ts
+++ test/app.controller.spec.ts
@@ -1,17 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApiController } from '../src/api/api.controller';
+import { AppService } from '../src/app.service';
 
 describe('AppController', () => {
-  let appController: AppController;
+  let appController: ApiController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
+      controllers: [ApiController],
       providers: [AppService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appController = app.get<ApiController>(ApiController);
   });
 
   describe('root', () => {
